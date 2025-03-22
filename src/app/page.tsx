e2c22c394e3ef386c78fe0d9e12a8286ab9c459c
peter1024 <u0052017@gmail..com>
2025-03-22 17:28:45 +0800

    feat: useContext classNames css theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,53 @@
 "use client";
-import { useMemo, useRef, useState } from "react";
+import { useContext, useMemo, useRef, useState } from "react";
 import axios from "axios";
 import type { User } from "./types/user";
 import UserCard from "./components/UserCard";
+import { ThemeContext } from "./contexts/all";
+import classNames from "classnames";
+
+const MyCard1 = () => {
+  const theme = useContext(ThemeContext);
+  return <>
+    <div className={classNames('p-2 w-[200px]',{
+      'bg-black text-white': theme === 'dark',
+      'bg-white text-black': theme === 'light',
+    })}>
+      MyCard1: {theme}
+    </div>
+  </>
+}
+
+const MyCard2 = () => {
+  const theme = useContext(ThemeContext);
+  return <>
+    <div className={classNames('p-2 w-[200px]',{
+      'bg-black text-white': theme === 'dark',
+      'bg-white text-black': theme === 'light',
+    })}>
+      MyCard2: {theme}
+    </div>
+  </>
+}
 
 export default function Home() {
-  const [list, setList] = useState<number[]>([1,2,3,4,5]);
-  const list2 = useMemo(() => list.map((i) => i*2), [list])
 
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  const [text, setText] = useState('')
-  const handleChange = (e) => {
-    const {value, name} = e.target
-    setText(value)
-  }
-
-  const add = () => {
-    const num = parseInt(text)
-    console.log(num, typeof num);
-    
-    if (num < 0 || num > 100) {
-      console.log('number must between 0 <= num <= 100');
-      
-      setText('')
-      focus()
+  const [theme, setTheme] = useState('dark');
+  const changeTheme = () => {
+    if (theme === 'dark') {
+      setTheme('light')
       return
     }
-    setList([
-      ...list,
-      num
-    ])
-    setText('')
-    focus()
-  }
-  
-  const focus = () => {
-    inputRef.current && inputRef.current.focus()
-  }
 
-  const handleKeyDown = (e) => {
-    const {code} = e
-    if (code === 'Enter') {
-      add()
-    }
-    
+    setTheme('dark')
   }
   return (
     <>
-      <input type="text" name="number" value={text} onChange={handleChange} ref={inputRef} onKeyDown={handleKeyDown}/>
-      <button type="button" onClick={add}>增加</button>
-      <p>{list.join(",")}</p>
-      <p>{list2.join(",")}</p>
+      <button type="button" onClick={changeTheme}>改變主題</button>
+      <ThemeContext.Provider value={theme}>
+        <MyCard1 />
+        <MyCard2 />
+      </ThemeContext.Provider>
     </>
   );
 }
