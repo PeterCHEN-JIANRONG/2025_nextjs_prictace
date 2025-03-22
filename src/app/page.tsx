7db59904505daae7d07969982384643b7a426816
peter1024 <u0052017@gmail..com>
2025-03-22 11:30:32 +0800

    feat: useContext createContext 修改Context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,58 @@
 "use client";
-import { ReactNode, useState } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 
 export default function Home() {
   // const [count, setCount] = useState(0)
-  
 
-  const Card = (props: {children?: ReactNode}) => {
-    return (<div className="p-2 bg-white border-2 border-blue-500 text-black">
-      <h1>我是卡片</h1>
-      { props.children }
-    </div>)
+  interface User {
+    name: string
+    age: number
+  }
+  
+  const ThemeContext = createContext<User>({
+    name: 'peter',
+    age: 20
+  });
+
+  const Header = (props: any) => {
+    const {name, age} = useContext(ThemeContext);
+    const click = () => {
+      props.set({
+        name: 'ho',
+        age: 30
+      })
+    }
+    return <div>
+      我是 Header
+      <p>{name} ---- {age}</p>
+      <button type="button" onClick={click}>按我</button>
+    </div>
+  }
+  const Content = (props:any) => {
+    const {name, age} = useContext(ThemeContext);
+    const click = () => {
+      props.set({
+        name: 'bubu',
+        age: 40
+      })
+    }
+    return <div>
+      我是 Content
+      <p>{age} === {name}</p>
+      <button type="button" onClick={click}>按我</button>
+    </div>
   }
 
+  const [user, setUser] = useState<User>({
+    name: '',
+    age: 0
+  })
+
   return (<>
-    <Card >
-      <p className="text-sm italic">子元素</p>
-    </Card>
-    <Card >
-      <p className="text-xl font-bold">子元素2</p>
-    </Card>
-    <Card />
+    
+    <ThemeContext.Provider value={user}>
+      <Header set={setUser}/>
+      <Content set={setUser}/>
+    </ThemeContext.Provider>
   </>)
 }
