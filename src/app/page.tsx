5a97bd5dd2681c768f60eca8de9de976255f97bc
peter1024 <u0052017@gmail..com>
2025-03-22 15:41:33 +0800

    feat: await async useState useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,38 @@
 "use client";
-import { createContext, ReactNode, useContext, useRef, useState } from "react";
+import { createContext, ReactNode, useContext, useEffect, useRef, useState } from "react";
+import axios from "axios";
+
+interface User {
+  name: {
+    title: 'Ms' | 'Mr'
+    first: string
+    last: string
+  }
+  email: string
+  phone: string
+  gender: 'female' | 'male'
+}
 
 export default function Home() {
-  // const [count, setCount] = useState(0)
+  const [user, setUser] = useState<User>()
 
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  const click = () => {
-    inputRef.current && inputRef.current.focus()
+  const getUser = async () => {
+    try {
+      const { data } = await axios('https://randomuser.me/api/')
+      console.log(data);
+      setUser(data.results[0])
+    } catch (error) {
+      console.log(error);
+    }
   }
 
+  useEffect(() => {
+    getUser()
+  },[])
+
   return (<>
-    <input type="text"  ref={inputRef} />
-    <button type="button" onClick={click}>點我</button>
+    <h1>{user?.name.title} {user?.name.last} {user?.name.first}</h1>
+    <p>{user?.email}</p>
+    <p>{user?.phone}</p>
   </>)
 }
