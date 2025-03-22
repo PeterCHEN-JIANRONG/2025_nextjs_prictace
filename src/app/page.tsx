2ef68930bada12abc0f807e85e7d66845b9a3f98
peter1024 <u0052017@gmail..com>
2025-03-22 11:50:40 +0800

    feat: useRef input focus()

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,17 @@
 "use client";
-import { createContext, ReactNode, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useRef, useState } from "react";
 
 export default function Home() {
   // const [count, setCount] = useState(0)
 
-  const list = [
-    {
-      name: 'peter',
-      age: 20
-    },
-    {
-      name: 'john',
-      age: 25
-    }
-  ]
+  const inputRef = useRef<HTMLInputElement>(null)
 
-  const say = (str: string) => {
-    console.log(`ih, my name is ${str}`);
+  const click = () => {
+    inputRef.current && inputRef.current.focus()
   }
 
   return (<>
-  <ul>
-    {
-      list.map((i)=><li key={i.age}>
-        {i.name}
-        <button type="button" onClick={() => say(i.name)} className="ml-4 p-1 bg-white text-black">sayHi</button>
-      </li>)
-    }
-  </ul>
+    <input type="text"  ref={inputRef} />
+    <button type="button" onClick={click}>點我</button>
   </>)
 }
