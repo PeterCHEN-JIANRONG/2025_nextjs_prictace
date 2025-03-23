37521a3737e52af073880d260d47a6f1c736096a
peter1024 <u0052017@gmail..com>
2025-03-23 11:19:24 +0800

    feat: useReducer practice2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,57 @@
 "use client";
 
-import { useReducer, useRef, useState } from "react";
+import { ChangeEvent, useReducer, useState } from "react";
 
-
-interface Counter {
-  count: number
+interface State {
+  count: number;
 }
 
-interface CounterAction {
-  type: 'add' | 'reduce',
-  number: number
-}
+type CounterAction =
+  | { type: "reset" }
+  | { type: "set"; value: State["count"] }
 
-const reducer = (state: Counter, action: CounterAction) => {
-  const {type, number} = action
-  console.log(action)
-  if (type === 'add') {
-    return {
-      count: state.count + number
-    }
+const initialState: State = { count: 0 };
+const countReducer = (state: State, action: CounterAction): State => {
+  switch(action.type) {
+    case 'reset':
+      return initialState
+    case 'set':
+      return {
+        ...state,
+        count: action.value
+      }
+    default:
+      throw new Error('Unknown action')
   }
-  if (type === 'reduce') {
-    return {
-      count: state.count - number
-    }
-  }
-
-  throw Error('Unknown action.');
 }
 
 export default function Counter() {
+  const [count, setCount] = useState(0);
+  const change = (e: ChangeEvent<HTMLInputElement>) => {
+    const num = e.target.value as string;
+    setCount(parseInt(num) || 0);
+  };
 
-  const change = (e: {target: HTMLInputElement}) => {
-    const {value} = e.target
-    setNumber(parseInt(value) || 0)
+  const [counter, counterDispatch] = useReducer(countReducer, initialState);
+  const reset = () => {
+    counterDispatch({type: 'reset'})
   }
-
-  const [number, setNumber] = useState<number>(0)
-  const [counter, dispatch] = useReducer(reducer, {count: 0})
   const add = () => {
-    dispatch({type: 'add', number})
-    setNumber(0)
-    focus()
+    counterDispatch({type: 'set', value: counter.count + count})
   }
   const reduce = () => {
-    dispatch({type: 'reduce', number})
-    setNumber(0)
-    focus()
-  }
-
-  const inputRef = useRef<HTMLInputElement>(null)
-  const focus = () => {
-    inputRef.current && inputRef.current.focus()
+    counterDispatch({type: 'set', value: counter.count - count})
   }
 
   return (
     <>
-      <p className="text-3xl font-bold">{counter.count}</p>
-      <div>
-        <input type="text" name="count" id="count" value={number} onChange={change} ref={inputRef} />
-        <button type="button" onClick={add}>增加</button>
-        <button type="button" onClick={reduce}>減少</button>
+      <h1 className="text-3xl font-bold">useReducer practice</h1>
+      <p className="text-2xl font-bold h-12">{counter.count}</p>
+      <div className="space-x-2">
+        <input type="text" name="count" value={count} onChange={change} />
+        <input type="button" value="reset" onClick={reset} className="bg-blue-500 py-1 px-2 rounded cursor-pointer hover:bg-blue-400" />
+        <input type="button" value="add" onClick={add} className="bg-blue-500 py-1 px-2 rounded cursor-pointer hover:bg-blue-400" />
+        <input type="button" value="reduce" onClick={reduce} className="bg-blue-500 py-1 px-2 rounded cursor-pointer hover:bg-blue-400" />
       </div>
     </>
   );
